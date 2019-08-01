8704e8e9e2d4409fa304d02b07d65b51122924b8
Hailey <hailey@athnex.com>
2019-07-31 20:21:51 -0700

    Removed and Reformatting
    Removed some checks that will never happen and redid the formatting of the console output.

--- csv-rater.py
+++ csv-rater.py
@@ -4,16 +4,27 @@ import csv
 import re
 import sys
 import ConfigParser
+
 config = ConfigParser.ConfigParser()
 config.read('config.ini')
-
+cfgDebug = config.get("main","debug")
 cdrfile = config.get("rater","cdr-file")
 monitoredExtension = config.get("rater","extension")
 monitoredNumber = config.get("rater","inbound-number")
+
 print "The arguments are: " , str(cdrfile)
 print "Looking for calls to and from", monitoredExtension, "as well as", monitoredNumber
+
 callTotal = 0
 callDuration = 0
+
+def sendDebug(debugMsg):
+    "Debug Function that checks if ini is set true for debug"
+    if cfgDebug == "true":
+        print debugMsg
+    return
+
+sendDebug( "!!!DEBUG ENABLED!!!")
 with open(cdrfile, 'rb') as csvfile:
     cdrHandle = csv.reader(csvfile, delimiter=',', quotechar='"')
     # The CDR Layout is as follows, <template name="example">"${caller_id_name}","${caller_id_number}","${destination_number}",
@@ -38,24 +49,21 @@ with open(cdrfile, 'rb') as csvfile:
             if re.match('^\+?1?(\d{5,10})$',cdrDestNumber) and re.match('^\+?1?(\d{5,10})$',cdrIdNumber):
                  #This is for local call matching
                 if re.match('^(\d{5})$',cdrIdNumber) and re.match('^(\d{5})$',cdrDestNumber):
-                    print "*Local call", cdrIdNumber, "calls", cdrDestNumber, "using", cdrCodec, "for", cdrDuration, "(", cdrBillsec, ") seconds,"
+                    sendDebug("Ignoring local call " + cdrIdNumber + " calls " + cdrDestNumber + " for " + cdrDuration + "(" + cdrBillsec + ")(" + cdrCodec + ")")
                     continue
                 
                 if cdrIdNumber == monitoredExtension:
                     callDuration = callDuration + int(cdrDuration)
                     callTotal = callTotal + int(cdrBillsec)
-                    print "!Extension", str(cdrIdNumber), " Calls ", cdrDestNumber, " for ", cdrDuration, "(", cdrBillsec, ") seconds, hangup was defined as", cdrHangupCause, "using", cdrCodec
-                    
-#                elif cdrIdNumber == monitoredNumber:
-#                    callDuration = callDuration + int(cdrDuration)
-#                    callTotal = callTotal + int(cdrBillsec)
-#                    print "!Source number", cdrIdNumber, "calls", cdrDestNumber, " for ", cdrDuration, "(", cdrBillsec, ") seconds, hangup was defined as", cdrHangupCause, "using", cdrCodec
-                    
+                    sendDebug("Outbound call, " + cdrIdNumber + " calls " + cdrDestNumber + " for " + cdrDuration + "(" + cdrBillsec + ")(" + cdrCodec + ")")
+
                 elif cdrDestNumber == monitoredExtension:
                     callDuration = callDuration + int(cdrDuration)
                     callTotal = callTotal + int(cdrBillsec)
-                    print "!Number", str(cdrIdNumber), " Calls ", cdrDestNumber, " for ", cdrDuration, "(", cdrBillsec, ") seconds, hangup was defined as", cdrHangupCause, "using", cdrCodec
- 
-callMinutes = callTotal / 60
-callRemainderSeconds = callTotal % 60
-print "Total Call length is", callDuration, "seconds, but billable is", callMinutes, "minutes and", callRemainderSeconds, "seconds."
+                    sendDebug("Inbound call " + cdrIdNumber + " calls " + cdrDestNumber + " for " + cdrDuration + "(" + cdrBillsec + ")(" + cdrCodec + ")")
+                     
+callMinutes = str(callTotal / 60)
+callRemainderSeconds = str(callTotal % 60)
+print "Total Call length is " + str(callDuration) + " seconds, but billable is " + callMinutes + " minutes and " + callRemainderSeconds + " seconds."
+
+
